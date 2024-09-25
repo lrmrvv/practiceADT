7d9347081ba99351594e7a58799bcdf08bbf099a
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2024-09-25 09:04:21 +0500

    Update DiscountCalculator.py

--- DiscountCalculator.py
+++ DiscountCalculator.py
@@ -3,18 +3,18 @@ class DiscountCalculator(ABC):
     def apply_discount(self, total):
         pass
 
-class NoDiscount(DiscountCalculator):
+class No(DiscountCalculator):
     def apply_discount(self, total):
         return total
 
-class PercentageDiscount(DiscountCalculator):
+class Percentage(DiscountCalculator):
     def __init__(self, percentage):
         self.percentage = percentage
 
     def apply_discount(self, total):
         return total - (total * self.percentage / 100)
 
-class FixedAmountDiscount(DiscountCalculator):
+class Fixed(DiscountCalculator):
     def __init__(self, discount_amount):
         self.discount_amount = discount_amount
 
